6265d23e15864a75d8edff600bae6f4a842ad399
sanjitdev <sanjit.majumdar@bs-23.net>
2024-05-03 20:08:16 +0600

    test husky

---
+++ src/app/features/employees/employees/employees.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  standalone: true,
+  imports: [],
+  templateUrl: './employees.component.html',
+  styleUrl: './employees.component.scss'
+})
+export class EmployeesComponent implements OnInit{
+  ngOnInit(): void {
+    console.log('');  
+  }
+  var
+}

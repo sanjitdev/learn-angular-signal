971f0c7ca6565d70a8ae3534274159407032a2c0
sanjitdev <sanjit.majumdar@bs-23.net>
2024-05-03 18:50:23 +0600

    adding signals to projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IsMarriedPipe } from './is-married.pipe';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+interface Person {
+  name: string;
+  email: string;
+  phone: string;
+  age: number;
+  married: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IsMarriedPipe, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'learn-angular-signal';
+export class AppComponent implements OnInit {
+  personsSignal = signal<Person[]>([
+    {
+      name: 'Sanjit Majumdar',
+      email: 'sanjit@gmail.com',
+      phone: '+984234243234',
+      age: 29,
+      married: false
+    }
+  ]);
+
+  totalPersonSignal = computed(() => this.personsSignal().length);
+
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+    age: new FormControl(18, Validators.required),
+    married: new FormControl(false)
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit(): void {
+    const newPerson: Person = this.form.getRawValue() as Person;
+
+    this.personsSignal.update(persons => {
+      return [
+        ...persons,
+        newPerson
+      ]
+    });
+
+    this.form.reset();
+  }
 }
---
+++ src/app/is-married.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'isMarried',
+  standalone: true
+})
+export class IsMarriedPipe implements PipeTransform {
+
+  transform(value: boolean, ...args: any[]): string {
+    return value ? 'Married' : 'Happy';
+  }
+
+}
